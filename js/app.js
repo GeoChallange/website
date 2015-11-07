5be7705ac9d714e5dea5ffdf3a1fcccaa40de126
Arne <git@arnefeil.de>
2015-11-07 13:45:49 +0100

    quest directive

--- js/app.js
+++ js/app.js
@@ -2,15 +2,24 @@
  * Created by HelmutCarmen on 06.11.2015.
  */
 angular.module('app', ['ngRoute', 'ui.bootstrap', 'leaflet-directive'])
-.config(function ($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'templates/index.view.html',
-    controller: 'BrowseController'
-  })
-    .when('/create', {
-      templateUrl: 'templates/create.view.html'
+    .config(function ($routeProvider) {
+        $routeProvider.when('/', {
+                templateUrl: 'templates/index.view.html',
+                controller: 'BrowseController'
+            })
+            .when('/create', {
+                templateUrl: 'templates/create.view.html',
+                controller: 'CreateController'
+            })
+            .otherwise({
+                redirect: '/'
+            })
     })
-    .otherwise({
-      redirect: '/'
-    })
-});
+    .directive('questStep', function () {
+        return {
+            templateUrl: 'templates/quest.view.html',
+            scope: {
+                quest: '=quest'
+            }
+        };
+    });
--- js/createController.js
+++ js/createController.js
@@ -3,6 +3,28 @@
 
     angular.module('app')
         .controller('CreateController', function ($scope, API) {
+            $scope.challenge = {};
 
+            $scope.challenge.quests = [{
+                lat: 59.91,
+                lon: 10.75
+            }];
+            $scope.addQuest = function () {
+                $scope.challenge.quests.push({});
+                console.debug($scope.challenge.quests);
+            };
+
+            $scope.$on('leafletDirectiveMarker.dragend', function (e, args) {
+                console.debug('adksapdja', e, args);
+                $scope.challenge.quests[0].lat = args.model.lat;
+                $scope.challenge.quests[0].lon = args.model.lng;
+
+            });
+
+            $scope.$on('leafletDirectiveMap.click', function (e, args) {
+                console.debug(e, args);
+                $scope.challenge.quests[0].lat = args.leafletEvent.latlng.lat;
+                $scope.challenge.quests[0].lon = args.leafletEvent.latlng.lng;
+            })
         });
 })();
