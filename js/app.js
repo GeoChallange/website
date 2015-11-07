a70099130469eb52dce3d5a0bba74240809e8e4a
Arne <git@arnefeil.de>
2015-11-07 19:50:21 +0100

    nice map

--- js/app.js
+++ js/app.js
@@ -29,4 +29,31 @@ angular.module('app', ['ngRoute', 'ui.bootstrap', 'ui.bootstrap.datetimepicker',
             }
         };
     })
+    .directive('map', function ($window) {
+        var setMap = function (scope, element, attrs) {
+            var mobile = $window.innerWidth < 992;
+            if (!mobile) {
+                console.debug(element.parent()[0].offsetWidth);
+                element.css('position', 'fixed');
+                element.css('top', '80px');
+                element.css('left', '50%');
+                element.css('width', element.parent()[0].offsetWidth + 'px');
+                element.css('padding-bottom', '0');
+            } else {
+                element.css('position', 'relative');
+                element.css('top', '0');
+                element.css('left', '0');
+                element.css('width', 'auto');
+                element.css('padding-bottom', '20px');
+            }
+        };
+        return function (scope, element, attrs) {
+            angular.element($window).bind('resize', function (e) {
+                scope.$apply(function () {
+                    setMap(scope,element,attrs);
+                })
+            });
+            setMap(scope,element,attrs);
+        };
+    })
     .value('URL', 'http://x.x.x.x:8080');
